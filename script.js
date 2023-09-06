2f1e895c0af4cc6e498d8f2eeab207b9c86ae1dd
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-09-06 11:05:43 +0200

    add startGame and changeScreen functions
    Switches screen when starting game and when pressing return button in game-screen

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const startScreen = document.querySelector(".start-screen");
+const gameScreen = document.querySelector(".game-screen");
+const startGameButtons = document.querySelectorAll(".game-mode button");
 
+const returnButton = document.querySelector(".return");
+
+const startGame = () => {
+  changeScreen();
+};
+startGameButtons.forEach((button) =>
+  button.addEventListener("click", startGame)
+);
+
+const changeScreen = () => {
+  startScreen.classList.toggle("off");
+  gameScreen.classList.toggle("off");
+};
+returnButton.addEventListener("click", changeScreen);
