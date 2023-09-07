5ddea0ad4f7ee547323bd3579061af4e33dc01e0
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-09-07 08:36:00 +0200

    Add renderGameBoard function
    takes GameBoard objects array and displays it on the screen

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const startScreen = document.querySelector(".start-screen");
 const gameScreen = document.querySelector(".game-screen");
+const gameBoardDisplay = document.querySelector(".game-board");
 const startGameButtons = document.querySelectorAll(".game-mode button");
-
 const returnButton = document.querySelector(".return");
+
+const GameBoard = (() => {
+  const gameBoard = [
+    ["X", "O", "X"],
+    ["X", "O", "O"],
+    ["O", "X", "X"],
+  ];
+  return { gameBoard };
+})();
+
+// const player = () => {
+//     pass;
+// }
+
+// const displayController = (() => {
+//   pass;
+// })()
 
 const startGame = () => {
   changeScreen();
+  renderGameBoard(GameBoard.gameBoard);
 };
+
 startGameButtons.forEach((button) =>
   button.addEventListener("click", startGame)
 );
@@ -17,10 +36,12 @@ const changeScreen = () => {
 };
 returnButton.addEventListener("click", changeScreen);
 
-const Gameboard = (() => {
-  const gameboard = [
-    ["X", "O", "X"],
-    ["X", "O", "O"],
-    ["O", "X", "X"],
-  ];
-})();
+const renderGameBoard = (gameBoard) => {
+  for (const row of gameBoard) {
+    for (const col of row) {
+      square = document.createElement("div");
+      square.classList.add("square");
+      gameBoardDisplay.append(square);
+    }
+  }
+};
